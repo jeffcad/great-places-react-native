d04cb6c69900354b425484a8df9eaa5f71a31143
Jeff Cadieux <jeffcadieux@hotmail.com>
2021-01-24 11:07:00 +0900

    Initial commit: expo init

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, StatusBar } from 'react-native'
 
-export default function App() {
+import Colors from './constants/Colors'
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor={Colors.primary}
+      />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
